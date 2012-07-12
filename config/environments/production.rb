fb00ec13c903ba8dbd7aa0f97bc0cb6168946d29
Brian Benvenisty <brian.benvenisty@gmail.com>
2012-07-12 14:40:11 -0400

    Changed production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ SampleApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #!!!!! BB changed below from false to true to get Heroku working (maybe)
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
