97fdbc741e93d0beaf7a2cdd1f4cad522f2d1314
Brian Benvenisty <brian.benvenisty@gmail.com>
2012-07-12 14:27:55 -0400

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ SampleApp::Application.routes.draw do
   
   get "users/new"
   
-  match '/', to: 'static_pages#home'
+  # match '/', to: 'static_pages#home'
   
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
