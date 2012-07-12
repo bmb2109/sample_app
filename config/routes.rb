2c64e19cac7506e410d0f3448355286bc79883ae
Brian Benvenisty <brian.benvenisty@gmail.com>
2012-07-12 14:18:48 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  
 
-  match root to: 'static_pages#home'
+  root to: 'static_pages#home'
+  
+  get "users/new"
+  
+  match '/', to: 'static_pages#home'
   
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
