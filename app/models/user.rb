a424d0761d83cf6cf650d810c27916ff5699f22f
Brian Benvenisty <brian.benvenisty@gmail.com>
2012-08-01 13:02:26 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
   before_save { self.email.downcase! }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -24,4 +26,10 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  
+  private
+    
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
